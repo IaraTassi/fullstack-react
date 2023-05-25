6be04b1a15e3e5db0287da6c076c8d6ab58ea87d
Iara Tassi <iaratassi@hotmail.com>
2023-05-24 22:17:59 -0300

    Projeto web

---
+++ alura-books-web/src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../img/livro.png'
+
+export const livros = [
+  {nome: 'Liderança em Design', id: 1, src:livro}, 
+  {nome: 'Liderança em UX', id: 1, src:livro},
+  {nome: 'JavaScript Assertivo', id: 2, src:livro}, 
+  {nome: 'Cangaceiros em javaScript', id: 3, src:livro}, 
+  {nome: 'Aventureiros do Java', id: 4, src:livro}, 
+]
